dbbff8b6e7a436fec425281a20322c9b2441974b
Chabare <chabare95@gmail.com>
2018-01-07 14:28:04 +0100

    Subclass rar from telegram.Bot

--- main.py
+++ main.py
@@ -10,11 +10,13 @@ from telegram.bot import Bot
 from telegram.ext import CommandHandler, Updater
 
 
-class RockAmRing:
+class RockAmRing(Bot):
     bot = None
 
-    def __init__(self, token):
-        self.bot = Bot(token)
+    def __init__(self, token: str):
+        if not token or token == "<YOUR_TOKEN>":
+            raise ValueError("`token` must have a valid value ({} given).".format(token))
+        super().__init__(token)
 
     @staticmethod
     def get_band_names() -> List[str]:
@@ -30,33 +32,33 @@ class RockAmRing:
 
         return band_names
 
-    def send_bands(self, bands: Iterable[str]):
-        self.bot.send_message(chat_id=id, text=self.get_sendable_bands_string(bands),
-                              parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
+    def send_bands(self, uid: str, bands: Iterable[str]):
+        self.send_message(chat_id=uid, text=self.get_sendable_bands_string(bands),
+                          parse_mode=telegram.ParseMode.MARKDOWN, disable_web_page_preview=True)
 
     @staticmethod
-    def write_bands(bands: Iterable[str], id: str):
-        with open("bands_{}".format(id), "w+") as fd:
+    def write_bands(uid: str, bands: Iterable[str]):
+        with open("bands_{}".format(uid), "w+") as fd:
             fd.write("\n".join(bands))
 
-    def get_bands(self, id: str) -> List[str]:
+    def get_bands(self, uid: str) -> List[str]:
         bands = self.get_band_names()
 
-        self.write_bands(bands, id)
+        self.write_bands(uid, bands)
 
         return bands
 
     @staticmethod
-    def get_old(id: str) -> List[str]:
+    def get_old(uid: str) -> List[str]:
         try:
-            with open("bands_{}".format(id), "r") as old_bands_fd:
+            with open("bands_{}".format(uid), "r") as old_bands_fd:
                 return [band.strip() for band in old_bands_fd.readlines()]
         except OSError:
             return []
 
-    def get_new(self, id: str) -> Dict[str, set]:
-        old_bands = self.get_old(id)
-        current_bands = self.get_bands(id)
+    def get_new(self, uid: str) -> Dict[str, set]:
+        old_bands = self.get_old(uid)
+        current_bands = self.get_bands(uid)
         removed = set()
 
         for old_band in old_bands:
@@ -67,8 +69,8 @@ class RockAmRing:
 
         return {"new": new_bands, "removed": removed}
 
-    def get_removed(self, id: str) -> List[str]:
-        return list(self.get_new(id)['removed'])
+    def get_removed(self, uid: str) -> List[str]:
+        return list(self.get_new(uid)['removed'])
 
     @staticmethod
     def get_band_url(band_name: str, check=True) -> Optional[str]:
@@ -96,43 +98,44 @@ class RockAmRing:
         return "\n".join([self.get_telegram_url_markdown(band, self.get_band_url(band)) for band in bands])
 
     def bands(self, update):
-        bands = self.get_bands(update.message.chat_id)
+        uid = update.message.chat_id
+        bands = self.get_bands(uid)
 
         if bands:
-            self.send_bands(bands)
+            self.send_bands(uid, bands)
         else:
-            self.bot.send_message(chat_id=update.message.chat_id, text="Bisher wurden keine Bands announced.")
+            self.send_message(chat_id=update.message.chat_id, text="Bisher wurden keine Bands announced.")
 
     def new_bands(self, update):
-        bands = list(self.get_new(update.message.chat_id)['new'])
+        uid = update.message.chat_id
+        bands = list(self.get_new(uid)['new'])
         if bands:
-            self.send_bands(bands)
+            self.send_bands(uid, bands)
         else:
-            self.bot.send_message(chat_id=update.message.chat_id, text="Es wurden keine neuen Bands announced.")
+            self.send_message(chat_id=update.message.chat_id, text="Es wurden keine neuen Bands announced.")
 
     def removed_bands(self, update):
-        bands = list(self.get_new(update.message.chat_id)['removed'])
+        uid = update.message.chat_id
+        bands = list(self.get_new(uid)['removed'])
         if bands:
-            self.send_bands(bands)
+            self.send_bands(uid, bands)
         else:
-            self.bot.send_message(chat_id=update.message.chat_id, text="Es wurden keine Bands abgesagt.")
+            self.send_message(chat_id=uid, text="Es wurden keine Bands abgesagt.")
 
-    # noinspection PyMethodMayBeStatic
     def start(self, update):
-        with open("bands_{}".format(update.message.chat_id), "bw+") as fd:
-            fd.write(b"")
+        uid = update.message.chat_id
+        self.write_bands(uid, [])
 
 
 def sched_new(rar: RockAmRing):
-    import re
     import os
     for file in os.listdir("."):
         try:
-            id = re.findall(r"bands_(.*)", file)[0]
-            if id:
-                bands = rar.get_new(id)['new']
+            uid = re.findall(r"bands_(.*)", file)[0]
+            if uid:
+                bands = rar.get_new(uid)['new']
                 if bands:
-                    rar.send_bands(bands)
+                    rar.send_bands(uid, bands)
         except IndexError:
             pass
 
@@ -151,7 +154,7 @@ if __name__ == "__main__":
             token = json.load(f)['token']
 
         rar = RockAmRing(token)
-        updater = Updater(bot=rar.bot)
+        updater = Updater(bot=rar)
         dispatcher = updater.dispatcher
 
         schedule(rar)
@@ -161,7 +164,7 @@ if __name__ == "__main__":
         dispatcher.add_handler(CommandHandler("abgesagt", rar.removed_bands))
         dispatcher.add_handler(CommandHandler("start", rar.start))
         dispatcher.add_handler(
-            CommandHandler("status", lambda b, u: b.send_message(chat_id=u.message.chat_id, text="Ok")))
+            CommandHandler("status", lambda b, u: b.send_message(chat_id=u.message.chat_uid, text="Ok")))
 
         updater.start_polling()
     except Exception as e:
